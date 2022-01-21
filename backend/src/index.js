46c10416f581f280a297a93b227d18d7796cfa2b
Matheus Todao <todaomatheus@hotmail.com>
2022-01-21 18:09:22 -0300

    add environment variable (PORT)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const proxies = require('./middlewares/proxy');
@@ -10,4 +11,5 @@ app.use(express.json());
 app.use(cors);
 app.use(proxies);
 
-app.listen(3001);
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`running ${port}`));
